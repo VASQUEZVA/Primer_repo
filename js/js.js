2957697597e61d37b8428839f838fcf818f6f634
Victor Vasquez <v.avasquez433@gmail.com>
2023-05-01 23:47:58 -0500

    fourt version

---
+++ js/js.js
@@ -0,0 +1,10 @@
+
+let btn = document.getElementById("botton1")
+
+
+function saludar(){
+
+    alert("Bienvenido a la profesion mas enrredada del mundo, pero es divertida y se gana bien")
+}
+
+btn.addEventListener("click", saludar)
